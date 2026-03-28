c87850743d5bd751caf641653ab3c6c4f06747fa
readever <readever@163.com>
2026-03-28 20:06:28 +0800

    init test

---
+++ test.py
@@ -0,0 +1,9 @@
+import simplestart as ss
+
+# Your web app starts here
+ss.write("Hello, world")
+ss.divider()
+ss.text("Create your web app with SimpleStart using Python.", tag="b")
+ss.space("mb-4")
+ss.text("More help at ")
+ss.link("http://www.simplestart.cc", href="http://www.simplestart.cc", target="_blank")
