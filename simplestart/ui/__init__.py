8e6f0e7f2a10594fc8c4f18a1f510b40479c7147
readever <readever@163.com>
2026-03-29 09:01:27 +0800

    init

---
+++ simplestart/ui/__init__.py
@@ -0,0 +1,98 @@
+from .card import card, section, container
+from .rate import rate
+from .button import button, button_group
+from .link import link
+from .icon import icon, bootstrap_icon, icon_with_bg
+from .space import space
+from .splitter import splitter
+from .text import text
+from .checkbox import checkbox, checkboxes
+from .autocomplete import autocomplete
+from .input import input, text_input
+from .radio import radio
+from .selectbox import selectbox
+from .slider import slider
+from .switch import switch
+from .upload import upload
+from .avatar import avatar
+from .badge import badge
+from .carousel import carousel
+from .collapse import collapse
+from .descriptions import descriptions
+from .empty import empty
+from .image import image
+from .table import table
+from .grid import grid
+from .columns import columns
+from .alert import alert
+from .dialog import dialog
+from .messagebox import messagebox
+from .tabs import tabs
+from .affix import affix
+from .breadcrumb import breadcrumb
+from .anchor import anchor
+from .panel import panel
+from .divider import divider
+from .listbox import listbox
+from .htmlview import htmlview
+from .menu import menu
+from .video import video
+from .audio import audio
+from .overlay import overlay
+from .markdown import markdown, md, mdpy
+from .write import write
+from .colorpicker import colorpicker
+from .popconfirm import popconfirm
+from .layout import row, col
+
+#不太成熟，以后再弄
+##from .echarts import echarts
+
+__all__ = [
+    'card', 'section', 'container',
+    'rate',
+    'button', 'button_group',
+    'link',
+    'icon', 'bootstrap_icon', 'icon_with_bg',
+    'space',
+    'splitter',
+    'text',
+    'checkbox', 'checkboxes',
+    'autocomplete',
+    'input', 'text_input',
+    'radio',
+    'selectbox',
+    'slider',
+    'switch',
+    'upload',
+    'avatar',
+    'badge',
+    'carousel',
+    'collapse',
+    'descriptions',
+    'empty',
+    'image',
+    'table',
+    'grid',
+    'columns',
+    'alert',
+    'dialog',
+    'messagebox',
+    'tabs',
+    'affix',
+    'breadcrumb',
+    'anchor',
+    'panel',
+    'divider',
+    'listbox',
+    'htmlview',
+    'menu',
+    'video',
+    'audio',
+    'overlay',
+    'markdown', 'md', 'mdpy',
+    'write',
+    'colorpicker',
+    'popconfirm',
+    'row', 'col'
+]
