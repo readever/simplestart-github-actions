9c66c72b082605c3e06613333866c350726d0cdc
readever <readever@163.com>
2026-04-13 14:55:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ for file in system_files:
 
 setup(
     name="simplestart",
-    version="0.0.1.44",
+    version="0.0.1.48",
     author="wy",
     description="build web app using python",
     ext_modules=extensions,
